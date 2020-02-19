63d31a8c3f2f15fd02cfc2ddd464729604a1df77
Eric Wong <ericwong12@live.com>
2020-02-18 19:16:00 -0700

    Coded function for view department Missing the final part of displaying each proper employee

--- Employee_Tracker_Schema.sql
+++ Employee_Tracker_Schema.sql
@@ -4,25 +4,39 @@ CREATE DATABASE Employee_Tracker_DB;
 USE Employee_Tracker_DB;
 
 CREATE TABLE Department(
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
-);
+	department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)ENGINE=INNODB;
 CREATE TABLE Role(
-	id INT NOT NULL AUTO_INCREMENT,
+	role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY(id)
-);
+    department_id INT NOT NULL
+)ENGINE=INNODB;
 CREATE TABLE Employee(
-	id INT NOT NULL AUTO_INCREMENT,
+	employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    mangager_id INT,
-    PRIMARY KEY(id)
-);
-SELECT * FROM Department;
-SELECT * FROM ROLE;
-SELECT * FROM Employee;
+    manager_id INT
+)ENGINE=INNODB;
+
+-- Creating Departments
+INSERT INTO Department(name)
+VALUES("Sales");
+INSERT INTO Department(name)
+VALUES("Engineering");
+INSERT INTO Department(name)
+VALUES("Finance");
+INSERT INTO Department(name)
+VALUES("Legal");
+
+
+-- Creating Roles
+INSERT INTO Role(title, salary, department_id)
+VALUES("Sales Lead", 100000,1),("Sales Person", 65000,2),("Lead Engineer", 120000,3),("Manager Engineer", 110000,4);
+
+
+-- Creating employees
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES("Eric","Wong",1, NULL),("Elizabeth","Osenbach",2, 1),("Eva","Trinh",3, 2),("Tony","Trinh",4,3);
--- employeeTracker.js
+++ employeeTracker.js
@@ -68,10 +68,13 @@ function startTracker(){
         }
     });
 }
+//Displays the employee cards
 function viewEmployee(){
+    //selects all, it is displaying in order by the console.table
+    // Ask about why this doesnt work (Employee.employee_id, Employee.first_name, Employee.last_name, Role.title, Department.name, Role.salary, Employee.manager_id)
     let query = "SELECT * ";
     query += "FROM Employee as EM INNER JOIN Role as RL ON EM.role_id = RL.role_id ";
-    query += "INNER JOIN ";
+    query += "INNER JOIN Department as DP ON RL.department_id = DP.department_id";
     connection.query(query, function(err, res){
         for(let i = 0; i < res.length; i++){
             console.table([
@@ -79,13 +82,55 @@ function viewEmployee(){
                     Employee_ID: res[i].employee_id,
                     First_Name: res[i].first_name,
                     Last_Name: res[i].last_name,
-                    Role_ID: res[i].role_id,
-                    Manager_ID: res[i].manager_id,
                     Title: res[i].title,
-                    Salary: res[i].salary
+                    Department: res[i].name,
+                    Salary: res[i].salary,
+                    //Manager id needs a manager table
+                    Manager_ID: res[i].manager_id
                 }
             ])
         }
         startTracker();
+    });
+}
+//Viewing only the department
+function viewDepartment(){
+    let query = "SELECT * FROM Department"
+    connection.query(query,function(err, res){
+
+   //Displays the department options
+        inquirer.prompt({
+            name: "viewDep",
+            type: "list",
+            message: "Here are the departments",
+            choices: function(){
+                let departmentArray = [];
+                for (let i = 0; i < res.length; i++){
+                    departmentArray.push(res[i].name);
+                }
+                return departmentArray;
+            }
+        }).then(function(choices){
+            console.log(choices);
+        // With the option chosen, It should spit out the choice that was made
+            let query = "SELECT * ";
+            query += "FROM Employee as EM INNER JOIN Role as RL ON EM.role_id = RL.role_id ";
+            query += "INNER JOIN Department as DP ON RL.department_id = DP.department_id ";
+            query += "WHERE DP.name = ?"
+            connection.query(query, [ choices.name ], function(err, res){
+                console.log(choices.name);
+                for(let i = 0; i < res.length; i++){
+                    console.table([
+                        {
+                            Employee_ID: res[i].employee_id,
+                            First_Name: res[i].first_name,
+                            Last_Name: res[i].last_name,
+                            Department: res[i].name,
+                        }
+                    ])
+                }
+                startTracker();
+            });
+        });
     });
 }
